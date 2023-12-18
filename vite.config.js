f76d4b842ebedcde8757cab84c53f0efc6cd60c5
CClotho <124051470+CClotho@users.noreply.github.com>
2023-12-18 23:45:58 +0800

    Added network access for mobile

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/* import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -6,3 +6,15 @@ export default defineConfig({
  
   plugins: [react()],
 })
+ */
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: '0.0.0.0'
+  },
+  plugins: [react()],
+});
